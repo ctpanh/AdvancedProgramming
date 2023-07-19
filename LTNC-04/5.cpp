b8b2a873f4140c2a64fa827d5b4ef2a7e8f4ff11
Anh Cao <ctpanh03@gmail.com>
2023-07-19 10:52:43 +0700

    LTNC-04 Mystery Number

---
+++ LTNC-04/5.cpp
@@ -0,0 +1,34 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int n;
+    cin >> n;
+    int a[n], b[n + 1];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    for (int i = 0; i < n + 1; i++)
+    {
+        cin >> b[i];
+    }
+    sort(a, a + n);
+    sort(b, b + n + 1);
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i] != b[i])
+        {
+            cout << b[i];
+            return 0;
+        }
+    }
+    cout << b[n];
+    return 0;
+}
