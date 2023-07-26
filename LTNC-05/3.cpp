af1eb5a030dbcc49b162bac9c51fc1ce4324d505
Anh Cao <ctpanh03@gmail.com>
2023-07-26 09:26:07 +0700

    LTNC-05 Vector-Erase

---
+++ LTNC-05/3.cpp
@@ -0,0 +1,29 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> v;
+    for (int i = 0; i < n; i++) {
+        int temp;
+        cin >> temp;
+        v.push_back(temp);
+    }
+    int x;
+    cin >> x;
+    v.erase(v.begin() + x - 1);
+    int a, b;
+    cin >> a >> b;
+    v.erase(v.begin() + a - 1, v.begin() + b - 1);
+    cout << v.size() << "\n";
+    for (int i = 0; i < v.size(); i++) {
+        cout << v.at(i) << " ";
+    }
+    return 0;
+}
