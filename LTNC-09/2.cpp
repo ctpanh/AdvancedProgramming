2582f1e359b7f669e7485c54cd9eecb75e469649
Anh Cao <ctpanh03@gmail.com>
2023-08-09 08:40:04 +0700

    LTNC-09 Day 1: Data Types

---
+++ LTNC-09/2.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <iomanip>
+#include <limits>
+
+using namespace std;
+
+int main() {
+    int i = 4;
+    double d = 4.0;
+    string s = "HackerRank ";
+
+    
+    // Declare second integer, double, and String variables.
+    int num_i;
+    double num_d;
+    string str;
+    // Read and save an integer, double, and String to your variables.
+    cin >> num_i;
+    cin >> num_d;
+    getline(cin >> ws, str); 
+    // Note: If you have trouble reading the entire string, please go back and review the Tutorial closely.
+    
+    // Print the sum of both integer variables on a new line.
+    cout << i + num_i << endl;
+    // Print the sum of the double variables on a new line.    
+    cout << setprecision(1) << fixed <<d + num_d << endl;
+    // Concatenate and print the String variables on a new line
+    // The 's' variable above should be printed first.
+    cout << s + str << endl;
+    
+
+    return 0;
+}
